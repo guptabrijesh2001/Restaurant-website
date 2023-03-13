7e6775093a80f00bd17827c24f22953cb4afb969
unknown <guptabrijesh2001@gmail.com>
2023-03-13 20:02:01 +0530

    third commit

--- src/component/Basics/Navbar.js
+++ src/component/Basics/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = ({Filteritem,menulist}) => {
       <div className="btn-group">
         {
             menulist.map((curElem)=>{
-                return <button className="btn-group__items" onClick={()=>Filteritem(curElem)}>{curElem}</button> //here we can not write curElem.catgory because we are rendering the whole cards onClick,we are not rendering buttons
+                return <button className="btn-group__item" onClick={()=>Filteritem(curElem)}>{curElem}</button> //here we can not write curElem.catgory because we are rendering the whole cards onClick,we are not rendering buttons
             })
         }
 
